329f6366b9b1281a678785f0c8e13e5413a7e9aa
Cristoval Wenoch <wenochcristoval@gmail.com>
2024-05-05 14:15:11 +0700

    feat: revamp modal UI

--- src/layouts/AddItemModal.js
+++ src/layouts/AddItemModal.js
@@ -1,7 +1,8 @@
-import { buildMainUI, refreshMainUI } from "./ScreenController";
+import { refreshMainUI } from "./ScreenController";
 import { todoController } from "../components/TodoController";
+import { format, isPast } from "date-fns";
 
-export function buildModal(lists) {
+export function buildModal() {
   const dialogAddItem = document.createElement('dialog');
   dialogAddItem.classList.add('dialog');
   dialogAddItem.classList.add('dialog-add-item__form');
@@ -10,18 +11,40 @@ export function buildModal(lists) {
   form.classList.add('form');
   form.classList.add('dialog-add-item__form');
 
+  // Header
+  const formHeader = document.createElement('div');
+  formHeader.classList.add('form__header');
+
+  const formHeading = document.createElement('h2');
+  formHeading.classList.add('form__heading');
+  formHeading.textContent = 'Add a task';
+
+  formHeader.appendChild(formHeading);
+  form.appendChild(formHeader);
+
   // Title
   const fieldTitle = document.createElement('div');
   fieldTitle.classList.add('form__field');
 
   const labelTitle = document.createElement('label');
+  labelTitle.classList.add('form__label');
   labelTitle.setAttribute('for', 'item-title');
   labelTitle.textContent = 'Task title';
 
   const inputTitle = document.createElement('input');
+  inputTitle.classList.add('input');
+  inputTitle.classList.add('form__input');
+  inputTitle.classList.add('form__input-title');
   inputTitle.type = 'text';
   inputTitle.id = 'item-title';
   inputTitle.required = true;
+  inputTitle.addEventListener('focusout', (e) => {
+    if (isTitleValid(e.target.value)) {
+      e.target.classList.remove('form__input--invalid');
+    } else {
+      e.target.classList.add('form__input--invalid');
+    }
+  });
 
   fieldTitle.appendChild(labelTitle);
   fieldTitle.appendChild(inputTitle);
@@ -32,58 +55,89 @@ export function buildModal(lists) {
   fieldDescription.classList.add('form__field');
 
   const labelDescription = document.createElement('label');
+  labelDescription.classList.add('form__label');
   labelDescription.setAttribute('for', 'item-description');
   labelDescription.textContent = 'Task description';
 
-  const inputDescription = document.createElement('input');
-  inputDescription.type = 'text';
+  const inputDescription = document.createElement('textarea');
+  inputDescription.classList.add('input');
+  inputDescription.classList.add('input--textarea');
+  inputDescription.classList.add('form__input');
+  inputDescription.classList.add('form__input-description');
+  inputDescription.classList.add('form__input--textarea');
   inputDescription.id = 'item-description';
 
   fieldDescription.appendChild(labelDescription);
   fieldDescription.appendChild(inputDescription);
   form.appendChild(fieldDescription);
 
-  // Due
-  const fieldDue = document.createElement('div');
-  fieldDue.classList.add('form__field');
+  // Due Date
+  const fieldDueDate = document.createElement('div');
+  fieldDueDate.classList.add('form__field');
 
-  const labelDue = document.createElement('label');
-  labelDue.setAttribute('for', 'item-due');
-  labelDue.textContent = 'Due date'
+  const labelDueDate = document.createElement('label');
+  labelDueDate.classList.add('form__label');
+  labelDueDate.setAttribute('for', 'item-due');
+  labelDueDate.textContent = 'Due date'
   
-  const inputDue = document.createElement('input');
-  inputDue.type = 'date';
-  inputDue.id = 'item-due';
+  const inputDueDate = document.createElement('input');
+  inputDueDate.classList.add('input');
+  inputDueDate.classList.add('input--date');
+  inputDueDate.classList.add('form__input');
+  inputDueDate.classList.add('form__input-due-date');
+  inputDueDate.classList.add('form__input--date');
+  inputDueDate.min = format(new Date(), 'yyyy-MM-dd');
+  inputDueDate.type = 'date';
+  inputDueDate.id = 'item-due';
+  inputDueDate.addEventListener('focusout', (e) => {
+    if (isDueDateValid(e.target.value)) {
+      e.target.classList.remove('form__input--invalid');
+    } else {
+      e.target.classList.add('form__input--invalid');
+    }
+  })
 
-  fieldDue.appendChild(labelDue);
-  fieldDue.appendChild(inputDue);
-  form.appendChild(fieldDue);
+  fieldDueDate.appendChild(labelDueDate);
+  fieldDueDate.appendChild(inputDueDate);
+  form.appendChild(fieldDueDate);
+
+  // Actions
+  const formActions = document.createElement('div');
+  formActions.classList.add('form__actions');
+  form.appendChild(formActions);
 
   // Button Add Item
   const btnAddItem = document.createElement('button');
+  btnAddItem.classList.add('button');
+  btnAddItem.classList.add('button--primary');
+  btnAddItem.classList.add('form__button');
   btnAddItem.type = 'submit';
-  btnAddItem.textContent = 'Add item';
+  btnAddItem.textContent = 'Add';
   btnAddItem.addEventListener('click', (e) => {
     e.preventDefault();
-    if (!form.checkValidity()) {
-      alert('Form is invalid');
-    } else {
+    if (isTitleValid(inputTitle.value) && isDueDateValid(inputDueDate.value)) {
       btnAddItemHandler(
         inputTitle.value,
         inputDescription.value,
-        inputDue.value,
+        inputDueDate.value,
       );
       form.reset();
     }
   });
-  form.appendChild(btnAddItem);
+  formActions.appendChild(btnAddItem);
 
   // Button Cancel
   const btnCancel = document.createElement('button');
+  btnCancel.classList.add('button');
+  btnCancel.classList.add('button--tertiary');
+  btnCancel.classList.add('form__button');
   btnCancel.type = 'button';
   btnCancel.textContent = 'Cancel';
-  btnCancel.addEventListener('click', close);
-  form.appendChild(btnCancel);
+  btnCancel.addEventListener('click', () => {
+    btnCancelHandler();
+    form.reset();
+  });
+  formActions.appendChild(btnCancel);
 
   dialogAddItem.appendChild(form);
   document.body.appendChild(dialogAddItem);
@@ -94,7 +148,7 @@ export function show() {
   dialogAddItem.showModal();
 }
 
-function close() {
+function btnCancelHandler() {
   const dialogAddItem = document.querySelector('.dialog-add-item__form');
   dialogAddItem.close();
 }
@@ -106,5 +160,13 @@ function btnAddItemHandler(title, description, dueDate) {
     dueDate,
   )
   refreshMainUI();
-  close();
+  btnCancelHandler();
+}
+
+function isTitleValid(title) {
+  return Boolean(title);
+}
+
+function isDueDateValid(dateString) {
+  return !isPast(dateString);
 }
