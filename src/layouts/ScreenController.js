36c3e887e8dc3281baff76805622252055f28314
Cristoval Wenoch <wenochcristoval@gmail.com>
2024-05-05 21:33:00 +0700

    wip: make edit item modal

--- src/layouts/ScreenController.js
+++ src/layouts/ScreenController.js
@@ -2,6 +2,11 @@ import { todoController } from '../components/TodoController.js';
 import * as AddItemModal from './AddItemModal.js';
 
 export function buildMainUI(contentDiv) {
+  todoController.addItemToActiveList("test", "", "");
+  todoController.addItemToActiveList("test2", "testdesc2", "");
+  todoController.addItemToActiveList("test3", "testdesc3", "2024-05-07");
+  todoController.getActiveList().getItem(0).toggleStarred();
+  todoController.getActiveList().getItem(1).toggleCompleted();
   const list = document.createElement('div');
   list.classList.add('list');
   contentDiv.appendChild(list);
@@ -65,15 +70,106 @@ function buildListItems() {
   listItemsContainer.classList.add('list__items-container');
 
   todoController.getActiveList().getAllItems().forEach((item, itemIndex) => {
+    const hasDescription = (item.getDescription()) ? true : false;
+    const isCompleted = item.getCompleted();
+    const isStarred = item.getStarred();
+
+    // Container
     const itemContainer = document.createElement('div');
     itemContainer.classList.add('item__container');
-
-    const itemTitle = document.createElement('h2');
-    itemTitle.classList.add('item__title');
-    itemTitle.textContent = item.getTitle();
-    itemContainer.appendChild(itemTitle);
+    itemContainer.dataset.itemIndex = itemIndex;
 
     listItemsContainer.appendChild(itemContainer);
+
+    // Completed
+    const itemCompleted = document.createElement('input');
+    itemCompleted.type = 'checkbox';
+    itemCompleted.classList.add('item__completed');
+    itemCompleted.checked = isCompleted;
+    
+    itemContainer.appendChild(itemCompleted);
+
+    // Item Input Container
+    const itemInputContainer = document.createElement('div');
+    itemInputContainer.classList.add('item__input-container');
+
+    itemContainer.appendChild(itemInputContainer);
+    
+    // Title
+    const itemTitle = document.createElement('input');
+    itemTitle.classList.add('input');
+    itemTitle.classList.add('item__input');
+    itemTitle.classList.toggle('item__input--completed', isCompleted);
+    itemTitle.value = item.getTitle();
+    itemTitle.addEventListener('focusout', () => {
+      if (itemTitle.value) {
+        item.setTitle(itemTitle.value);
+      } else {
+        itemTitle.value = item.getTitle();
+      }
+    });
+    itemTitle.addEventListener('keypress', (e) => {
+      if (e.key === 'Enter') {
+        if (itemTitle.value) {
+          item.setTitle(itemTitle.value);
+        } else {
+          itemTitle.value = item.getTitle();
+        }
+        document.activeElement.blur();
+      }
+    });
+
+    itemInputContainer.appendChild(itemTitle);
+
+    // Description
+    // const itemDescription = document.createElement('input');
+    // itemDescription.classList.add('input');
+    // itemDescription.classList.add('input--textarea');
+    // itemDescription.classList.add('item__input');
+    // itemDescription.type = 'textarea';
+    // itemDescription.value = item.getDescription();
+
+    // itemInputContainer.appendChild(itemDescription);
+
+    // Actions
+    const itemActionsContainer = document.createElement('div');
+    itemActionsContainer.classList.add('item__actions-container');
+
+    itemContainer.appendChild(itemActionsContainer);
+
+    // Due Date
+    const itemDueDate = document.createElement('input');
+    itemDueDate.classList.add('input');
+    itemDueDate.classList.add('item__input');
+    itemDueDate.classList.add('item__due-date');
+    itemDueDate.type = 'date';
+    itemDueDate.value = item.getDueDate();
+
+    itemActionsContainer.appendChild(itemDueDate);
+
+    // Item Star
+    const btnItemStar = document.createElement('button');
+    btnItemStar.classList.add('button');
+    btnItemStar.classList.add('button--tertiary');
+    btnItemStar.classList.add('button--icon');
+    btnItemStar.classList.add('item__button-star');
+    btnItemStar.classList.toggle('item__button-star--starred', isStarred);
+    btnItemStar.classList.toggle('las', isStarred);
+    btnItemStar.classList.toggle('lar', !isStarred);
+    btnItemStar.classList.add('la-star');
+    btnItemStar.addEventListener('click', () => {
+      item.toggleStarred();
+      btnItemStar.classList.toggle('item__button-star--starred');
+      btnItemStar.classList.toggle('las');
+      btnItemStar.classList.toggle('lar');
+    });
+
+    itemActionsContainer.appendChild(btnItemStar);
+
+    itemCompleted.addEventListener('change', () => {
+      item.toggleCompleted();
+      itemTitle.classList.toggle('item__title--completed');
+    });
   });
 
   return listItemsContainer;
